9399b08c85645042c59f2a139f7ba6ca88506c79
toddawhittaker <todd@thewhittakers.org>
2020-12-16 14:36:46 -0500

    Update MimirTestRunner.sh

--- MimirTestRunner.sh
+++ MimirTestRunner.sh
@@ -8,16 +8,16 @@ mv bin/* /bin
 for file in *.php; do
   status="$(php -l $file 2>&1)"
   if [ $? -ne 0 ]; then
-    echo "$status" > DEBUG
-    echo 0 > OUTPUT
+    echo "$status" >> DEBUG
+    echo 0 >> OUTPUT
     exit 1
   fi
 done
 
-phpunit.phar --log-teamcity output.log . > DEBUG
+phpunit.phar --log-teamcity output.log . >> DEBUG
 tests=$(grep 'testStarted' output.log | wc -l)
 failures=$(grep 'testFailed' output.log | wc -l)
 ((passing = $tests - $failures))
 ((score = $passing * 100 / $tests))
-echo "You passed $passing out of $tests, earning $score%% of points"
+echo "You passed $passing out of $tests, earning $score%% of points" >> DEBUG
 echo "$score" > OUTPUT
